15fac1cf73988d99b4df316cfcf7db2e1abdcf9a
Jeff Dederick <jdederick@ketch.partners>
2018-05-10 13:59:04 -0400

    code refactored and debugged

--- index.js
+++ index.js
@@ -4,69 +4,124 @@ const md5 = require('md5');
 let random = crypto.randomBytes(64);
 let hash = md5(random);
 
+//New Hashed Product-object class
 class HashedSku {
-  constructor(sku, hash) {
-    this.sku = sku;
+  constructor(material, sku, hash) {
+    this.MaterialNumber = material;
+    this.SSN = sku;
     this.hash = hash;
   }
 }
 
+//Stand-in Database
 let serialNumbers = [];
 
-const hasher = (input, alteration) => {
-  let num1 = 0,
-    num2 = 0,
-    val = alteration ? input.toString() + alteration : input.toString(),
+//Test if Material & Serial Number combination already exist in Database
+const existingSSNTest = (db, val) =>
+  db.some(e => `${e.MaterialNumber}-${e.SSN}` === val);
+
+//Test if newly generated hash value already exists in the Database
+const existingHashTest = (db, val) => db.some(e => e.hash === val);
+
+//Transform a letter into it's corresponding number
+const letterToNumber = val => {
+  return val.charCodeAt(0) - 96;
+};
+
+//Transform a number into it's corresponding letter
+const numberToLetter = val => {
+  return String.fromCharCode(96 + val).toUpperCase();
+};
+
+//Decides whether the next value in the new hash
+//will be a number or letter and invokes the appropriate
+//method.
+const hashCombiner = (val1, val2) => {
+  if (Number(val1) === 0 || Number(val1)) {
+    return newNumber(val1, val2);
+  }
+  return newLetter(val1, val2);
+};
+
+//Method that ensures that all values added to the
+//hash are a single digit only
+//-- Ensures the new hash will remain less than
+//18 characters in length
+const singleDigit = (val1, val2) => {
+  if (val1 + val2 >= 10) {
+    return Math.floor((val1 + val2) / 2);
+  }
+  return val1 + val2;
+};
+
+//Tests the conditions under which to call the
+//next method to generate the new number to be added to the
+//current hash.
+const newNumber = (val1, val2) => {
+  if (Number(val2) === 0 || Number(val2)) {
+    return singleDigit(Number(val1), Number(val2));
+  }
+  return singleDigit(Number(val1), letterToNumber(val2));
+};
+
+//Tests the conditions under which to call
+//the next method to generate the new letter
+// to be added to the current hash.
+const newLetter = (val1, val2) => {
+  if (Number(val2) === 0 || Number(val2)) {
+    return numberToLetter(letterToNumber(val1) + Number(val2));
+  }
+  return numberToLetter(letterToNumber(val1) + letterToNumber(val2));
+};
+
+//The main hashing function. Sets the needed
+//variables, iterates through the split serial-number,
+//and returns the updated and sorted Database value.
+const hasher = (material, input, alteration) => {
+  //required variables
+  let val = alteration
+      ? `${material.toString()}-${input.toString()}${alteration}`
+      : `${material.toString()}-${input.toString()}`,
     hash = md5(val),
-    str1 = hash.slice(0, hash.length / 2),
-    str2 = hash.slice(hash.length / 2),
-    arr1 = str1.split(''),
-    arr2 = str2.split(''),
+    arr1 = hash.slice(0, hash.length / 2).split(''),
+    arr2 = hash.slice(hash.length / 2).split(''),
     newHash = [];
 
-  if (serialNumbers.some(e => e.sku === input)) {
-    console.log('Serial number already exists');
+  //test if material/serial number comination already exists.
+  if (existingSSNTest(serialNumbers, val)) {
+    console.log(`Material number ${val} already exists`);
     return;
   }
 
+  //iterates through the split serial-number hashes.
+  //Adds the returned values to the current hash.
   for (let i = 0; i < arr1.length; i++) {
-    if (Number(arr1[i]) === 0 || Number(arr1[i])) {
-      num1 = Number(arr1[i]);
-      if (Number(arr2[i]) === 0 || Number(arr2[i])) {
-        num2 = Number(arr2[i]);
-      } else {
-        num2 = arr2[i].charCodeAt(0) - 96;
-      }
-      newHash.push(
-        num1 + num2 >= 10 ? Math.floor((num1 + num2) / 2) : num1 + num2
-      );
-    } else {
-      num1 = arr1[i].charCodeAt(0) - 96;
-      if (Number(arr2[i]) === 0 || Number(arr2[i])) {
-        num2 = Number(arr2[i]);
-      } else {
-        num2 = arr2[i].charCodeAt(0) - 96;
-      }
-      newHash.push(
-        String.fromCharCode(96 + Math.floor(num1 + num2)).toUpperCase()
-      );
-    }
+    newHash.push(hashCombiner(arr1[i], arr2[i]));
   }
 
-  if (serialNumbers.some(e => e.hash === newHash.join(''))) {
-    return hasher(input, 'S');
+  //tests if the newly generated hash already exists in the Database.
+  //Generates a new hash if it does.
+  if (existingHashTest(serialNumbers, newHash.join(''))) {
+    return hasher(material, input, 'S');
   }
-  serialNumbers.push(new HashedSku(input, newHash.join('')));
+
+  //Adds the new hashed product-object to the Database.
+  serialNumbers.push(new HashedSku(material, input, newHash.join('')));
+
+  //Re-sorts the Database to ensure quick searching in the future.
   serialNumbers = serialNumbers.sort();
-  console.log(serialNumbers);
-  return newHash.join('');
+
+  //returns the updated Database.
+  return serialNumbers;
 };
 
-hasher(hash);
-hasher('Jeffrey');
-hasher('129R734B243I9DSWNEI392374JH4328');
-hasher('Jeffrey');
-// console.log(md5(hash));
-// console.log(md5('jeffrey'));
-// console.log(md5('129R734B243I9DSWNEI392374JH4328'));
-// console.log(md5('jeffrey'));
+//Module Tests
+console.log(hasher('345ur', hash));
+console.log('==========================');
+console.log(hasher('345ur', 'Jeffrey'));
+console.log('==========================');
+console.log(hasher('345ur', '129R734B243I9DSWNEI392374JH4328'));
+console.log('==========================');
+console.log(hasher('345ur', 'Jeffrey'));
+console.log('==========================');
+console.log(hasher('29472354', 'Jeffrey'));
